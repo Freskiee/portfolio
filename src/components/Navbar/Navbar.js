485636d5eb89aa6ba8781c589f9f4856839cbdf0
Ariel Sanchez <freska_96@hotmail.com>
2024-07-13 19:35:15 -0600

    feat: add burger menu mobile and desktop

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,33 @@
 
+import { useState } from 'react';
 import * as S from './Navbar.styles';
 
 const Navbar = () => {
+
+    const [showMenuBurger, setShowMenuBurger] = useState(false);
+
     return (
-        <S.ContainerNavbar>Nuestro Navbar</S.ContainerNavbar>
+        <S.ContainerNavbar>
+            <S.ContainerLogo>
+                <S.ImageLogo src='/img/logo-dev.png' alt='logo' />
+                <div>
+                    <h1>Ariel Freskiee</h1>
+                    <h2>Frontend Developer</h2>
+                </div>
+            </S.ContainerLogo>
+
+            <S.ContainerBurger onClick={() => setShowMenuBurger(!showMenuBurger)}>
+                <S.BurgerMenu />
+                <S.BurgerMenu />
+                <S.BurgerMenu />
+            </S.ContainerBurger>
+
+            <S.ContainerItems showMenuBurger={showMenuBurger}>
+                <p>About me</p>
+                <p>Projects</p>
+                <S.Button>📲Contact me</S.Button>
+            </S.ContainerItems>
+        </S.ContainerNavbar >
     );
 };
 
